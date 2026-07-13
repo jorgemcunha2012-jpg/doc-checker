42c67b5f158f60583c00dbf01977296e90f0a632
Jorge Cunha <257895116+jorgemcunha2012-jpg@users.noreply.github.com>
2026-07-13 19:00:38 -0300

    Include canvas native package in serverless trace

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@napi-rs/canvas"],
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/@napi-rs/canvas/**/*",
+      "./node_modules/@napi-rs/canvas-linux-x64-gnu/**/*",
+      "./node_modules/@napi-rs/canvas-linux-x64-musl/**/*",
+    ],
+  },
   typedRoutes: true,
 };
 
