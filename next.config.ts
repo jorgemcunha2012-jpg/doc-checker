6ad8abe7e46d01ec99a7f7c5db6880a81068f335
Jorge Cunha <257895116+jorgemcunha2012-jpg@users.noreply.github.com>
2026-07-13 22:26:08 -0300

    Include Tesseract runtime dependencies in deployment

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,8 @@ const nextConfig: NextConfig = {
       "./node_modules/tesseract.js/src/worker/node/**/*",
       "./node_modules/tesseract.js/src/**/*",
       "./node_modules/tesseract.js-core/**/*",
+      "./node_modules/regenerator-runtime/**/*",
+      "./node_modules/is-url/**/*",
     ],
     "/api/validation-processes": [
       "./node_modules/tesseract.js/src/worker-script/node/**/*",
@@ -16,6 +18,8 @@ const nextConfig: NextConfig = {
       "./node_modules/tesseract.js/src/worker/node/**/*",
       "./node_modules/tesseract.js/src/**/*",
       "./node_modules/tesseract.js-core/**/*",
+      "./node_modules/regenerator-runtime/**/*",
+      "./node_modules/is-url/**/*",
     ],
   },
   typedRoutes: true,
